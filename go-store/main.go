5cbed552d52a12f800854f7da170fb603a0733b5
Gabriel Majivu <gmajivu@gmail.com>
2025-02-24 22:13:01 +0300

    drop in-place file operation
    replace data atomically by renaming files. its atomic wrt to concurrent
    readers - a reader opens either the old or new file ie they dont get
    half written data.
    
    if update is interrupted, you can recover from old file which is intact.
    
    renaming still affected by powerloss and not durable.
    
    renaming works because the file system keeps a mapping from file names
    to file data so renaming means pointing the file name to the new data
    without touching the old data. operation is constant time.
    
    on linux old file may still exists if opened by a reader and its not
    just accessible from filename. reader can work on the data version they
    have and writer is not blocked by reader.
    
    @todo: prevent concurrent writers (level of concurrency is
    multi-reader-single-writer)

--- go-store/main.go
+++ go-store/main.go
@@ -1,22 +1,32 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
 )
 
 func saveData(path string, data []byte) error {
-	println("save file to disk")
+	tmp := fmt.Sprintf("%s.tmp.%d", path, rand.Int())
 	fp, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0664)
 	if err != nil {
 		return err
 	}
-	defer fp.Close()
+	defer func() {
+		fp.Close()
+		if err != nil {
+			os.Remove(tmp)
+		}
+	}()
 
-	_, err = fp.Write(data)
-	if err != nil {
-		return nil
+	if _, err = fp.Write(data); err != nil {
+		return err
 	}
-	return fp.Sync()
+	if err = fp.Sync(); err != nil {
+		return err
+	}
+	err = os.Rename(tmp, path)
+	return err
 }
 
 func main() {
